4d304fb6d5bbcd800e4d42277fc728581e4753a5
Abhishek jha <100504807+abhishek010126@users.noreply.github.com>
2026-08-19 22:44:47 +0530

    Update finding.py

--- backend/finding.py
+++ backend/finding.py
@@ -1,13 +1,11 @@
 import streamlit as st
-import os
 
-from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
 
 
-load_dotenv()
+
 
 client = genai.Client(
     api_key=st.secrets["GEMINI_API_KEY"]
